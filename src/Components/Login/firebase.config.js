9efe4dc63ae9a0846539887c6e7894e49f877e3d
JannatunNahar98 <67545173+JannatunNahar98@users.noreply.github.com>
2021-01-26 17:33:11 +0600

    finish

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCc8qlgD34leEPTieGaYmi6q_G1tL6K_gQ",
+  authDomain: "travel-guru-19fd5.firebaseapp.com",
+  projectId: "travel-guru-19fd5",
+  storageBucket: "travel-guru-19fd5.appspot.com",
+  messagingSenderId: "16814405134",
+  appId: "1:16814405134:web:c5db03edee8dcd2787ee5f",
+  measurementId: "G-M0SC7MSBLC"
+};
+
+  export default firebaseConfig;
